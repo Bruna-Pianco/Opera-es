41f8f59432d47a56b900444917b87afb2662f40b
Bruna-Pianco <bruna.magalhaes02@hotmail.com>
2021-01-10 23:58:57 -0300

    commit inicial

---
+++ Arquivos/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EX02_OPERACOES
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1, n2, total; // 3 variaveis do tipo inteiro
+            n1 = 10;
+            n2 = 3;
+
+
+            Console.WriteLine("Realização de operações aritméticas com 2 valores inteiros\n");
+            total = n1 + n2;
+            Console.WriteLine(n1 + " + " + n2 + " = " + total);
+            Console.WriteLine("A soma de + n1 + );
+            Console.WriteLine(n1 + " - " + n2 + " = " + (n1 - n2));
+            Console.WriteLine(n1 + " x " + n2 + " = " + (n1 * n2));
+            Console.WriteLine(n1 + " / " + n2 + " = " + (n1 / n2));
+            Console.WriteLine(n1 + " % " + n2 + " = " + (n1 % n2));
+            total = n1 % n2;
+            Console.WriteLine(" O resto da divisão inteira " + n1 + " por " + n2 + " Resulta em " + total);
+            Console.Write("\n \n Pressione qualquer tecla para sair...");
+            Console.ReadKey();
+
+
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,10 @@
+#### Realizado durante o 1º Módulo da aula "Programação e Algoritmo"
+
+Para fixar o conteúdo sobre operações aritméticas em C#
+
+Professor: JOSÉ MANOEL PEREIRA ANTUNES
+
+ETEC Professor Basilides de Godoy
+
+
+
